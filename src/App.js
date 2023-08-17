393f601b1d3ce90cab26d2ce89c10bce48bc36d4
Mustakim <m.m.kiron@gmail.com>
2023-08-17 14:30:38 +0600

    Setup project for buidling calculator app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Calculator</p>
+        <p>1 + 1 = 2 😀</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
