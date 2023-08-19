0d326fcb6ce847493ecc7e77ee01d8aebcad142e
Mustakim <m.m.kiron@gmail.com>
2023-08-20 01:20:32 +0600

    Add quotes from API

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CalculatorButtons from './CalculatorButtons';
 import calculate from '../logic/calculate';
+import Quote from './Quote';
 import '../App.css';
 
 function Calculator() {
@@ -12,36 +13,41 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <div className="display">{calculation.next || calculation.total || '0'}</div>
-      <div className="button-row">
-        <CalculatorButtons label="AC" onClick={() => clickButtons('AC')} />
-        <CalculatorButtons label="+/-" onClick={() => clickButtons('+/-')} />
-        <CalculatorButtons label="%" onClick={() => clickButtons('%')} />
-        <CalculatorButtons label="÷" onClick={() => clickButtons('÷')} />
+    <div className="app-container">
+      <div className="quote-container">
+        <Quote />
       </div>
-      <div className="button-row">
-        <CalculatorButtons label="7" onClick={() => clickButtons('7')} />
-        <CalculatorButtons label="8" onClick={() => clickButtons('8')} />
-        <CalculatorButtons label="9" onClick={() => clickButtons('9')} />
-        <CalculatorButtons label="x" onClick={() => clickButtons('x')} />
-      </div>
-      <div className="button-row">
-        <CalculatorButtons label="4" onClick={() => clickButtons('4')} />
-        <CalculatorButtons label="5" onClick={() => clickButtons('5')} />
-        <CalculatorButtons label="6" onClick={() => clickButtons('6')} />
-        <CalculatorButtons label="-" onClick={() => clickButtons('-')} />
-      </div>
-      <div className="button-row">
-        <CalculatorButtons label="1" onClick={() => clickButtons('1')} />
-        <CalculatorButtons label="2" onClick={() => clickButtons('2')} />
-        <CalculatorButtons label="3" onClick={() => clickButtons('3')} />
-        <CalculatorButtons label="+" onClick={() => clickButtons('+')} />
-      </div>
-      <div className="button-row bottom-row">
-        <CalculatorButtons label="0" onClick={() => clickButtons('0')} />
-        <CalculatorButtons label="." type="narrow" onClick={() => clickButtons('.')} />
-        <CalculatorButtons label="=" type="narrow" onClick={() => clickButtons('=')} />
+      <div className="calculator">
+        <div className="display">{calculation.next || calculation.total || '0'}</div>
+        <div className="button-row">
+          <CalculatorButtons label="AC" onClick={() => clickButtons('AC')} />
+          <CalculatorButtons label="+/-" onClick={() => clickButtons('+/-')} />
+          <CalculatorButtons label="%" onClick={() => clickButtons('%')} />
+          <CalculatorButtons label="÷" onClick={() => clickButtons('÷')} />
+        </div>
+        <div className="button-row">
+          <CalculatorButtons label="7" onClick={() => clickButtons('7')} />
+          <CalculatorButtons label="8" onClick={() => clickButtons('8')} />
+          <CalculatorButtons label="9" onClick={() => clickButtons('9')} />
+          <CalculatorButtons label="x" onClick={() => clickButtons('x')} />
+        </div>
+        <div className="button-row">
+          <CalculatorButtons label="4" onClick={() => clickButtons('4')} />
+          <CalculatorButtons label="5" onClick={() => clickButtons('5')} />
+          <CalculatorButtons label="6" onClick={() => clickButtons('6')} />
+          <CalculatorButtons label="-" onClick={() => clickButtons('-')} />
+        </div>
+        <div className="button-row">
+          <CalculatorButtons label="1" onClick={() => clickButtons('1')} />
+          <CalculatorButtons label="2" onClick={() => clickButtons('2')} />
+          <CalculatorButtons label="3" onClick={() => clickButtons('3')} />
+          <CalculatorButtons label="+" onClick={() => clickButtons('+')} />
+        </div>
+        <div className="button-row bottom-row">
+          <CalculatorButtons label="0" onClick={() => clickButtons('0')} />
+          <CalculatorButtons label="." type="narrow" onClick={() => clickButtons('.')} />
+          <CalculatorButtons label="=" type="narrow" onClick={() => clickButtons('=')} />
+        </div>
       </div>
     </div>
   );
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,24 +1,58 @@
 import React, { useState, useEffect } from 'react';
 
-//API KEY WbOSWI37+aKQ9vX6ZHP+uA==QiFg4i75K9iPWB1U
+// API KEY WbOSWI37+aKQ9vX6ZHP+uA==QiFg4i75K9iPWB1U
 
 function Quote() {
+  const [quote, selectQuote] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, showError] = useState('');
 
-    useEffect(
+  useEffect(() => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=knowledge', {
+      headers: {
+        'X-Api-Key': 'WbOSWI37+aKQ9vX6ZHP+uA==QiFg4i75K9iPWB1U',
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Response was inaccurate');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const randomQuote = data[Math.floor(Math.random() * data.length)];
+        selectQuote(randomQuote);
+        setLoading(false);
+      })
+      .catch((error) => {
+        showError(error.message);
+        setLoading(false);
+      });
+  }, []);
 
-    );
+  if (loading) {
+    return <div className="loading">Loading the Quote</div>;
+  }
 
-    if (loading) {
-
-    }
-
-    if (error) {
-
-    }
-
+  if (error) {
     return (
+      <div className="error-message">
+        Error:
+        {error}
+      </div>
+    );
+  }
 
-    )
-};
+  return (
+    <div>
+      <h1>Knowledge Quote of the Moment</h1>
+      <p>{quote.quote}</p>
+      <h4>
+        ~
+        {quote.author}
+      </h4>
+    </div>
+  );
+}
 
 export default Quote;
