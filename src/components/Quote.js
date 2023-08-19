5ba58c926bea146c9393e11029b519479415bb59
Mustakim <m.m.kiron@gmail.com>
2023-08-19 23:39:21 +0600

    Create quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+//API KEY WbOSWI37+aKQ9vX6ZHP+uA==QiFg4i75K9iPWB1U
+
+function Quote() {
+
+    useEffect(
+
+    );
+
+    if (loading) {
+
+    }
+
+    if (error) {
+
+    }
+
+    return (
+
+    )
+};
+
+export default Quote;
