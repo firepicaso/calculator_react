6c25e5e5f04e84d8e5a52063da82890c52b8f265
Mustakim <m.m.kiron@gmail.com>
2023-08-25 11:36:31 +0600

    Add React routing in the app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/home';
+import Quote from './components/Quote';
+import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Calculator />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Math Magicians</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/calculator">Calculator</Link>
+              </li>
+              <li>
+                <Link to="/quote">Quote</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </div>
+    </Router>      
   );
 }
-
-export default App;
