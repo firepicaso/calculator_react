656c02331af620f071c70a9889a1cf2c9efec887
unknown <albertkantwi@gmail.com>
2023-08-26 08:35:48 +0100

    Add testing with react library

--- src/components/CalculatorButtons.test.js
+++ src/components/CalculatorButtons.test.js
@@ -1,3 +1,16 @@
 import CalculatorButtons from "./CalculatorButtons";
 import { render, screen, fireEvent } from '@testing-library/react';
 
+test('renders Button component', () => {
+    const { container } = render(<CalculatorButtons label="1" onClick={() => {}} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  
+  test('simulating user interaction', () => {
+    const clickButtons = jest.fn();
+    render(<CalculatorButtons label="1" onClick={clickButtons} />);
+    const buttonElement = screen.getByText('1');
+    fireEvent.click(buttonElement);
+    fireEvent.click(buttonElement);
+    expect(clickButtons).toHaveBeenCalledTimes(2);
+});
